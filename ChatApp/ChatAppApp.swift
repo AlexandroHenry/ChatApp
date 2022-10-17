fd6e7b84ac566f8ef38717cab5bee96a346ea827
alaxhenry <alaxhenry0121@gmail.com>
2022-10-17 13:09:20 +0900

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Seungchul Ha on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
