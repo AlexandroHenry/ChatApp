c90dc61b3ce63637e0c572604714830d224038dd
alaxhenry <alaxhenry0121@gmail.com>
2022-10-17 15:07:16 +0900

    Google Login 1

--- ChatApp/LoginView.swift
+++ ChatApp/LoginView.swift
@@ -6,6 +6,22 @@
 //
 
 import SwiftUI
+import Firebase
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        
+        super.init()
+    }
+}
 
 struct LoginView: View {
     @State var isLoginMode = false
@@ -58,6 +74,9 @@ struct LoginView: View {
                             Spacer()
                         } .background(.orange)
                     }
+                    
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.red)
                 }
                 .padding()
                 
@@ -66,13 +85,47 @@ struct LoginView: View {
             .background(Color(.init(white: 0, alpha: 0.05))
                 .ignoresSafeArea())
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     private func hanndleAction() {
         if isLoginMode {
             print("Should log into firebase with existing credentials")
+            loginUser()
         } else {
+            createNewAccount()
             print("Register a new account inside of firebase auth and then store image in storage somehow...")
+        }
+    }
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, error in
+            
+            if let err = error {
+                print("Failed to create user:", err)
+                self.loginStatusMessage = "Failed to login user: \(err)"
+                return
+            }
+            
+            print("Successfully logged in as user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully logged in user: \(result?.user.uid ?? "")"
+        }
+    }
+    
+    @State var loginStatusMessage = ""
+    
+    private func createNewAccount() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let err = error {
+                print("Failed to create user:", err)
+                self.loginStatusMessage = "Failed to create user: \(err)"
+                return
+            }
+            
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            
+            self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
         }
     }
 }
