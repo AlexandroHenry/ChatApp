13c960ffa996ae4e11e051f76a27e3d90a832afa
alaxhenry <alaxhenry0121@gmail.com>
2022-10-18 19:20:01 +0900

    Transition to Chat Log View when Click navigation Link

--- ChatApp/MainMessages/NewMessage/CreateNewMessageView.swift
+++ ChatApp/MainMessages/NewMessage/CreateNewMessageView.swift
@@ -42,6 +42,8 @@ class CreateNewMessageViewModel: ObservableObject {
 
 struct CreateNewMessageView: View {
     
+    let didSelectNewUser: (ChatUser) -> ()
+    
     @Environment (\.presentationMode) var presentationMode
     
     @ObservedObject var vm = CreateNewMessageViewModel()
@@ -54,9 +56,11 @@ struct CreateNewMessageView: View {
                 
                 ForEach(vm.users) { user in
                     Button {
-                         
+                        presentationMode.wrappedValue.dismiss()
+                        
+                        didSelectNewUser(user)
                     } label: {
-                        HStack {
+                        HStack(spacing: 16) {
                             WebImage(url: URL(string: user.profileImageUrl))
                                 .resizable()
                                 .scaledToFill()
